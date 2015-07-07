fb9dc6e4c91d69763b8fa48581b0ef73ec45d3a2
Yubenben <benben8901@126.com>
2015-07-07 18:55:24 +0800

    add

--- ForkViewDemo/app/src/main/java/com/ben/sample/forkviewdemo/MainActivity.java
+++ ForkViewDemo/app/src/main/java/com/ben/sample/forkviewdemo/MainActivity.java
@@ -1,38 +1,84 @@
 package com.ben.sample.forkviewdemo;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RelativeLayout;
+
+import com.ben.sample.forkviewdemo.widget.CartoonView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private RelativeLayout mCanveLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mCanveLayout = (RelativeLayout) findViewById(R.id.canve_layout);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        switch (item.getItemId()) {
+            case R.id.add_circle:
+                mCanveLayout.addView(newCartoonView(this, R.drawable.fav_search_location, false));
+                break;
+            case R.id.add_edit:
+                mCanveLayout.addView(newCartoonView(this, R.drawable.speech_vector, true));
+                break;
+            case R.id.add_squre:
+                mCanveLayout.addView(newCartoonView(this, R.drawable.app_attach_file_icon_music_large, false));
+                break;
+            case R.id.save:
+                //mCanveLayout.measure(View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED), View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+                //mCanveLayout.layout(0, 0, mCanveLayout.getMeasuredWidth(), mCanveLayout.getMeasuredHeight());
+                mCanveLayout.buildDrawingCache();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
+                        File file = new File(path, "demo1.png");
+                        try {
+                            path.mkdirs();
+                            OutputStream out = new FileOutputStream(file);
+                            mCanveLayout.getDrawingCache().compress(Bitmap.CompressFormat.PNG, 90, out);
+                            out.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            default:
+                break;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private CartoonView newCartoonView(Context context, int img_id, boolean editable) {
+        CartoonView cartoon = new CartoonView(context);
+        cartoon.setPictureResource(img_id);
+        cartoon.setEditable(editable);
+        return cartoon;
     }
 }
--- ForkViewDemo/app/src/main/java/com/ben/sample/forkviewdemo/widget/CartoonView.java
+++ ForkViewDemo/app/src/main/java/com/ben/sample/forkviewdemo/widget/CartoonView.java
@@ -25,111 +25,6 @@ public class CartoonView extends RelativeLayout {
     private ImageView mPicture;
     private ImageView mBorder;
     private EditText mEditText;
-    private View.OnTouchListener mTouchListener = new View.OnTouchListener() {
-
-        float centerX, centerY;
-        float downScaleX, downScaleY, downRotation;
-        double fromRadius, toRadius;
-        double fromAngle, toAngle;
-
-        @Override
-        public boolean onTouch(View v, MotionEvent event) {
-
-            switch (event.getAction()) {
-
-                case MotionEvent.ACTION_DOWN:
-
-                    final int[] location = new int[2];
-                    getLocationOnScreen(location);
-                    centerX = location[0] + getWidth() / 2;
-                    centerY = location[1] + getHeight() / 2;
-                    fromRadius = Math.sqrt(Math.pow((event.getRawX() - centerX), 2)
-                            + Math.pow((event.getRawY() - centerY), 2));
-                    fromAngle = Math.toDegrees(Math.atan((event.getRawY() - centerY)
-                            / (event.getRawX() - centerX)));
-                    downScaleX = mPicture.getScaleX();
-                    downScaleY = mPicture.getScaleY();
-                    downRotation = mPicture.getRotation();
-
-                    break;
-                case MotionEvent.ACTION_MOVE:
-                    toRadius = Math.sqrt(Math.pow((event.getRawX() - centerX), 2)
-                            + Math.pow((event.getRawY() - centerY), 2));
-                    toAngle = Math.toDegrees(Math.atan((event.getRawY() - centerY)
-                            / (event.getRawX() - centerX)));
-                    if (event.getRawX() < centerX) {
-                        toAngle += 180;
-                    }
-                    mPicture.setRotation((float) (toAngle - fromAngle)
-                            + downRotation);
-                    mPicture.setScaleX((float) ((toRadius / fromRadius) * downScaleX));
-                    mPicture.setScaleY((float) (toRadius / fromRadius) * downScaleY);
-
-                    mBorder.setScaleX((float)
-                                    (
-                                            ((toRadius / fromRadius) * downScaleX) *
-                                                    (
-                                                            mPicture.getWidth() * Math.abs(Math.cos(Math.toRadians(mPicture.getRotation())))
-                                                                    + mPicture.getHeight() * Math.abs(Math.sin(Math.toRadians(mPicture.getRotation())))
-                                                    )
-                                                    / mPicture.getWidth()
-                                    )
-                    );
-                    mBorder.setScaleY((float)
-                                    (
-                                            ((toRadius / fromRadius) * downScaleY) *
-                                                    (
-                                                            mPicture.getWidth() * Math.abs(Math.sin(Math.toRadians(mPicture.getRotation())))
-                                                                    + mPicture.getHeight() * Math.abs(Math.cos(Math.toRadians(mPicture.getRotation())))
-                                                    )
-                                                    / mPicture.getHeight()
-                                    )
-                    );
-
-                    sButton.setTranslationX((float)
-                                    (
-                                            (
-                                                    (
-                                                            mPicture.getWidth() * Math.abs(Math.cos(Math.toRadians(mPicture.getRotation())))
-                                                                    + mPicture.getHeight() * Math.abs(Math.sin(Math.toRadians(mPicture.getRotation())))
-                                                    )
-                                                            / mPicture.getWidth() * ((toRadius / fromRadius) * downScaleX)
-                                            )
-                                                    - 1
-                                    )
-                                    * (mPicture.getWidth() / 2)
-                    );
-                    sButton.setTranslationY((float)
-                                    (
-                                            (
-                                                    (
-                                                            mPicture.getWidth() * Math.abs(Math.sin(Math.toRadians(mPicture.getRotation())))
-                                                                    + mPicture.getHeight() * Math.abs(Math.cos(Math.toRadians(mPicture.getRotation())))
-                                                    )
-                                                            / mPicture.getHeight() * ((toRadius / fromRadius) * downScaleY)
-                                            )
-                                                    - 1
-                                    )
-                                    * (mPicture.getHeight() / 2)
-                    );
-
-                    mEditText.setMaxWidth((int) (mPicture.getWidth() * mPicture.getScaleX() - TEXT_PADDING_SIZE));
-                    mEditText.setMaxHeight((int) (mPicture.getHeight() * mPicture.getScaleY() - TEXT_PADDING_SIZE));
-
-                    break;
-                case MotionEvent.ACTION_UP:
-
-                    break;
-                case MotionEvent.ACTION_CANCEL:
-
-                    break;
-
-                default:
-                    break;
-            }
-            return true;
-        }
-    };
     private int mImageSourceId;
     private int mBackgroundId;
     private boolean mEditable;
@@ -167,23 +62,139 @@ public class CartoonView extends RelativeLayout {
         sButton = (ImageView) layout.findViewById(R.id.scale_btn);
         mEditText = (EditText) layout.findViewById(R.id.edit_text);
 
-        if (mImageSourceId != -1) {
-            mPicture.setImageResource(mImageSourceId);
-        }
-        if (mBackgroundId != -1) {
-            mBorder.setBackgroundResource(mBackgroundId);
-        }
-        if (mEditable) {
-            mEditText.setMaxWidth(mPicture.getWidth() - TEXT_PADDING_SIZE);
-            mEditText.setMaxHeight(mPicture.getHeight() - TEXT_PADDING_SIZE);
-        } else {
-            mEditText.setVisibility(View.GONE);
-        }
-        sButton.setOnTouchListener(mTouchListener);
+        setPictureResource(mImageSourceId);
+        setBorderResource(mBackgroundId);
+        setEditable(mEditable);
+        sButton.setOnTouchListener(new View.OnTouchListener() {
+
+            float centerX, centerY;
+            float downScaleX, downScaleY, downRotation;
+            double fromRadius, toRadius;
+            double fromAngle, toAngle;
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                switch (event.getAction()) {
+
+                    case MotionEvent.ACTION_DOWN:
+
+                        final int[] location = new int[2];
+                        getLocationOnScreen(location);
+                        centerX = location[0] + getWidth() / 2;
+                        centerY = location[1] + getHeight() / 2;
+                        fromRadius = Math.sqrt(Math.pow((event.getRawX() - centerX), 2)
+                                + Math.pow((event.getRawY() - centerY), 2));
+                        fromAngle = Math.toDegrees(Math.atan((event.getRawY() - centerY)
+                                / (event.getRawX() - centerX)));
+                        downScaleX = mPicture.getScaleX();
+                        downScaleY = mPicture.getScaleY();
+                        downRotation = mPicture.getRotation();
+
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        toRadius = Math.sqrt(Math.pow((event.getRawX() - centerX), 2)
+                                + Math.pow((event.getRawY() - centerY), 2));
+                        toAngle = Math.toDegrees(Math.atan((event.getRawY() - centerY)
+                                / (event.getRawX() - centerX)));
+                        if (event.getRawX() < centerX) {
+                            toAngle += 180;
+                        }
+                        mPicture.setRotation((float) (toAngle - fromAngle)
+                                + downRotation);
+                        mPicture.setScaleX((float) ((toRadius / fromRadius) * downScaleX));
+                        mPicture.setScaleY((float) (toRadius / fromRadius) * downScaleY);
+
+                        mBorder.setScaleX((float)
+                                        (
+                                                ((toRadius / fromRadius) * downScaleX) *
+                                                        (
+                                                                mPicture.getWidth() * Math.abs(Math.cos(Math.toRadians(mPicture.getRotation())))
+                                                                        + mPicture.getHeight() * Math.abs(Math.sin(Math.toRadians(mPicture.getRotation())))
+                                                        )
+                                                        / mPicture.getWidth()
+                                        )
+                        );
+                        mBorder.setScaleY((float)
+                                        (
+                                                ((toRadius / fromRadius) * downScaleY) *
+                                                        (
+                                                                mPicture.getWidth() * Math.abs(Math.sin(Math.toRadians(mPicture.getRotation())))
+                                                                        + mPicture.getHeight() * Math.abs(Math.cos(Math.toRadians(mPicture.getRotation())))
+                                                        )
+                                                        / mPicture.getHeight()
+                                        )
+                        );
+
+                        sButton.setTranslationX((float)
+                                        (
+                                                (
+                                                        (
+                                                                mPicture.getWidth() * Math.abs(Math.cos(Math.toRadians(mPicture.getRotation())))
+                                                                        + mPicture.getHeight() * Math.abs(Math.sin(Math.toRadians(mPicture.getRotation())))
+                                                        )
+                                                                / mPicture.getWidth() * ((toRadius / fromRadius) * downScaleX)
+                                                )
+                                                        - 1
+                                        )
+                                        * (mPicture.getWidth() / 2)
+                        );
+                        sButton.setTranslationY((float)
+                                        (
+                                                (
+                                                        (
+                                                                mPicture.getWidth() * Math.abs(Math.sin(Math.toRadians(mPicture.getRotation())))
+                                                                        + mPicture.getHeight() * Math.abs(Math.cos(Math.toRadians(mPicture.getRotation())))
+                                                        )
+                                                                / mPicture.getHeight() * ((toRadius / fromRadius) * downScaleY)
+                                                )
+                                                        - 1
+                                        )
+                                        * (mPicture.getHeight() / 2)
+                        );
+
+                        mEditText.setMaxWidth((int) (mPicture.getWidth() * mPicture.getScaleX() - TEXT_PADDING_SIZE));
+                        mEditText.setMaxHeight((int) (mPicture.getHeight() * mPicture.getScaleY() - TEXT_PADDING_SIZE));
+
+                        break;
+                    case MotionEvent.ACTION_UP:
+
+                        break;
+                    case MotionEvent.ACTION_CANCEL:
+
+                        break;
+
+                    default:
+                        break;
+                }
+                return true;
+            }
+        });
 
         addView(layout, new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT,
                 RelativeLayout.LayoutParams.MATCH_PARENT));
         return layout;
     }
 
+    public void setPictureResource(int reference) {
+        if (reference != -1) {
+            mPicture.setImageResource(reference);
+        }
+    }
+
+    public void setBorderResource(int reference) {
+        if (reference != -1) {
+            mPicture.setImageResource(reference);
+        }
+    }
+
+    public void setEditable(boolean editable) {
+        if (editable) {
+            mEditText.setVisibility(View.VISIBLE);
+            mEditText.setMaxWidth(mPicture.getWidth() - TEXT_PADDING_SIZE);
+            mEditText.setMaxHeight(mPicture.getHeight() - TEXT_PADDING_SIZE);
+        } else {
+            mEditText.setVisibility(View.GONE);
+        }
+    }
 }
